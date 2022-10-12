3272a405616a23c819eab340c7c6dfa81fa398a6
Jiyan Patil <jiyanpatil0707@gmail.com>
2022-10-12 10:47:11 +0530

    CodeChanged

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Jiyan patil</h1>
+        <h1>Jiyan Patil</h1>
         <p>
 
           Edit <code>src/App.js</code> and save to reload.
