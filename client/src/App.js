212aeadc82c7c839c9ef0a24d706ce61c8568a97
Jiyan Patil <jiyanpatil0707@gmail.com>
2022-10-15 16:29:41 +0530

    FrontedUpdated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core'
+import memories from './images/memories.png'
 
 const App =()=>  {
   return (
-    <div>App</div>
-  )
+      <Container>
+          <AppBar position="static" color='inherit'>
+            <Typography variant="h2" align='center'>
+                MemoriesApp
+            </Typography>
+            <img src={memories} alt='memories' height='60'/>
+
+          </AppBar>
+
+      </Container>
+
+
+    )
 }
 
 export default App;
--- server/controller/posts.js
+++ server/controller/posts.js
@@ -12,6 +12,16 @@ export const getPosts=async (req,res)=>{
 // res.send("ok")
 }
 
-export const createPost=(req,res)=>{
+export const createPost=async(req,res)=>{
+    const post=req.body
+    const newPost = new     PostMessage(post)
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+        
+    } catch (error) {
+        res.status(404).json({message:error.message})
+
+    }
 
 }
